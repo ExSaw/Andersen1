ccab9f0efb139b4d4a902f9033c8b841d0ffc4b4
Alexander Chudov <usatu.robotics@gmail.com>
2021-12-02 20:51:50 +0500

    added second textView

--- app/src/main/java/com/rickrip/andersen1/MainActivity.java
+++ app/src/main/java/com/rickrip/andersen1/MainActivity.java
@@ -3,12 +3,17 @@ package com.rickrip.andersen1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewTwo = findViewById(R.id.tvTwo);
+        textViewTwo.setText(R.string.textTwo);
     }
 }
